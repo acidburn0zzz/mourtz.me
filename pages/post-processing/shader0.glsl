ede25677e8b7e14862b44fb769530dab2730cb3f
Mourtz <alex.mourtziapis@gmail.com>
2015-09-03 21:06:52 +0300

    Update "post-processing" page.

--- pages/post-processing/shader0.glsl
+++ pages/post-processing/shader0.glsl
@@ -4,14 +4,15 @@ precision mediump float;
 
 #define SAMPLE 10.0
 
+uniform float u_sampling;
 uniform float u_time;
 uniform vec2 u_resolution;
 uniform sampler2D u_texture;
 varying vec2 v_texcoord;
 
 void main( void ) {
-    float dx = SAMPLE * (1./u_resolution.x);
-    float dy = SAMPLE * (1./u_resolution.y);
+    float dx = u_sampling * (1./u_resolution.x);
+    float dy = u_sampling * (1./u_resolution.y);
     vec2 texCoords = vec2(dx*floor(v_texcoord.x/dx), dy*floor(v_texcoord.y/dy));
 	
 	gl_FragColor =  texture2D(u_texture, texCoords);

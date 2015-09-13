8c52ebebad87964ab74723ce3677011ea38ae4c7
Mourtz <alex.mourtziapis@gmail.com>
2015-09-13 19:49:15 +0300

    Update "post-processing" page

--- pages/post-processing/shader3.glsl
+++ pages/post-processing/shader3.glsl
@@ -6,12 +6,13 @@ precision mediump float;
 
 uniform float u_time;
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform sampler2D u_texture;
 varying vec2 v_texcoord;
 
 void main( void ) {     
-    vec2 cntr = v_texcoord - 0.5;
-    vec2 uv = v_texcoord - (cntr/0.5)*tan(length(cntr)-u_time)*0.05;
-    vec3 col = texture2D(u_texture,uv).xyz;
-    gl_FragColor = vec4(col,1.0);  
+    vec2 cntr = v_texcoord - u_mouse/u_resolution;
+    vec2 uv = v_texcoord + cntr*tan(length(cntr)-u_time)*0.05;
+    
+    gl_FragColor = texture2D(u_texture,uv);  
 }

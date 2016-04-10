2601f6c417635f8323da9fa71ac38ee9efeb2c44
Mourtz <alex.mourtziapis@gmail.com>
2016-04-10 17:57:27 +0300

    Performance Improvements in "post-processing" page.

--- shaders/shader3.glsl
+++ shaders/shader3.glsl
@@ -6,32 +6,26 @@ uniform float u_time;
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform bool u_mouse_down;
+uniform int u_piano_tiles [32];
 
-#define bg_color vec3(0.5,0.9,1.0)
-#define black_tile_color vec3(0.0)
-#define start_tile_color vec3(0.0,0.5,0.7)
+
+const vec3 bg_color = vec3(0.5,0.9,1.0);
+const vec3 black_tile_color = vec3(0.0);
+const vec3 start_tile_color = vec3(0.0,0.5,0.7);
 
 #define tile_height u_resolution.y/4.0
 #define tile_width u_resolution.x/4.0
     
-vec2 tiles_pivots[16];
-
-int getTile(vec2 pos){
-    for(int i = 0; i < 16; i++){
-        if(pos.x >= tiles_pivots[i].x && pos.x < tiles_pivots[i].x + tile_width &&
-            pos.y >= tiles_pivots[i].y && pos.y < tiles_pivots[i].y + tile_height)
-            return i;
-    }
-}
-
-#define id getTile(gl_FragCoord.xy)
+vec2 tiles_pivots[32];
+int id = -1;
 
 int getTileWhereMouse(){
     for(int i = 0; i < 16; i++){
         if (u_mouse.x >= tiles_pivots[i].x && u_mouse.x < tiles_pivots[i].x + tile_width &&
             u_mouse.y >= tiles_pivots[i].y && u_mouse.y < tiles_pivots[i].y + tile_height)
             return i;
-    }       
+    }     
+    return -1;
 }
 
 void main( void ) {
@@ -39,18 +33,28 @@ void main( void ) {
     for(int x = 0; x < 4; x++){
         for(int y = 0; y < 4; y++){
             tiles_pivots[x*4+y] = vec2(x*int(tile_width), y*int(tile_height));
+
+            if(id == -1){
+                if(gl_FragCoord.x >= tiles_pivots[x*4+y].x && gl_FragCoord.x < tiles_pivots[x*4+y].x + tile_width &&
+                    gl_FragCoord.y >= tiles_pivots[x*4+y].y && gl_FragCoord.y < tiles_pivots[x*4+y].y + tile_height)
+                    id = x*4+y;
+            }
         }
     }
 
     vec3 color = bg_color;
     
-    // tiles
-    if(id == 4)
+    
+    // fist tile
+    if(id == 8)
         color = start_tile_color;
-    else if(id == 10)
-        color = black_tile_color;
-    else if(id == 3)
-        color = black_tile_color;
+
+    // tiles
+    for(int i = 0; i < 4; i++){
+        if(id == i*4+u_piano_tiles[i])
+            color = black_tile_color;
+    }
+
 
     int isMouseOver = getTileWhereMouse();
     if(id == isMouseOver && u_mouse_down)
